4ca9d1c2204673313696a27bb442d7308d548d81
Ramin Atayee <raminatayeeofficial@gmail.com>
2026-02-17 23:38:41 +0430

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const typingText = document.querySelector("typingText");
+const typingText = document.querySelector(".typingText");
 
 function randomParagraph(){
     let randTndex = Math.floor(Math.random() * paragraphs.length);
     paragraphs[randTndex].split("").forEach(span => {
-        let spanTag = '<span>${span}</span>';
-        // typingText.innerHTML = spanTag;
+        let spanTag =  `<span>${span}</span>`;
+        typingText.innerHTML += spanTag;
     });
 }
 randomParagraph();
